dd5c7ed92f3da36013fb7462299ef75496b5b49f
l1pman <nlipman@list.ru>
2022-09-28 23:14:17 +0700

    add models

--- balanced_diets/admin.py
+++ balanced_diets/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Topic, Entry
-admin.site.register(Topic)
-admin.site.register(Entry)
+
+from .models import Dish, Product, Recipe, User, exclusion_product
+admin.site.register(Dish)
+admin.site.register(Product)
+admin.site.register(Recipe)
+admin.site.register(User)
+admin.site.register(exclusion_product)
+
--- balanced_diets/models.py
+++ balanced_diets/models.py
@@ -1,21 +1,46 @@
 from django.db import models
 
 # Create your models here.
-class Topic(models.Model):
-    """Тема, которую изучает пользователь"""
-    text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        """Возвращает строковое представление модели."""
-        return self.text
 
-class Entry(models.Model):
-    """Информация, изученная пользователем по теме"""
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-class Meta:
-    verbose_name_plural = 'entries'
-def __str__(self):
-    """Возвращает строковое представление модели."""
-    return f"{self.text[:50]}..."
+class Dish(models.Model):
+    meals = [
+        ('B', 'Завтрак'),
+        ('L', 'Обед'),
+        ('D', 'Ужин')
+    ]
+    name = models.CharField(max_length=50)
+    kcal = models.DecimalField(max_digits=6, decimal_places=2)
+    proteins = models.DecimalField(max_digits=6, decimal_places=2)
+    fats = models.DecimalField(max_digits=6, decimal_places=2)
+    carbs = models.DecimalField(max_digits=6, decimal_places=2)
+    meal = models.CharField(max_length=15, choices=meals, default='')
+    lactose = models.BooleanField()
+    vegan = models.BooleanField()
+    diabetes = models.BooleanField()
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+
+class Recipe(models.Model):
+    id_dish = models.ForeignKey(Dish, on_delete=models.DO_NOTHING)
+    id_product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+
+class User(models.Model):
+    genders = [
+        ('M', 'Мужской'),
+        ('F', 'Женский')
+    ]
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=genders, default='')
+    weight = models.IntegerField()
+    height = models.IntegerField()
+    age = models.IntegerField()
+    activity = models.CharField(max_length=30)
+    lactose = models.BooleanField(default=False)
+    vegan = models.BooleanField(default=False)
+    diabetes = models.BooleanField(default=False)
+
+
+class exclusion_product(models.Model):
+    id_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    id_product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
